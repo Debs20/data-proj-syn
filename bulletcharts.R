a668472f41cee326cc361b91f9dcdd5e7a933eae
Debbie <debbielamb62@gmail.com>
2020-08-19 16:31:38 +0100

    bulletcharts

---
+++ bulletcharts.R
@@ -0,0 +1,46 @@
+
+library(plotly)
+
+#Goal 17 code
+goal_4 <- plot_ly() 
+goal_4 <- goal_4 %>%
+  add_trace(
+    type = "indicator",
+    mode = "number+gauge+delta",
+    value = 43,
+    delta = list(reference = 40),
+    domain = list(x = c(0.25, 1), y = c(0.4, 0.6)),
+    title = list(text = "Goal 17 Completion"),
+    gauge = list(
+      shape = "bullet",
+      axis = list(range = list(NULL, 200)),
+      threshold = list(
+        line = list(color = "red", width= 2),
+        thickness = 0.75,
+        value = 43),
+      steps = list(
+        list(range = c(0, 150), color = "lightgray"),
+        list(range = c(150, 200), color = "gray")),
+      bar = list(color = "green"))) 
+#Goal 13 code
+goal_4<- goal_4 %>%
+  add_trace(
+    type =  "indicator",
+    mode = "number+gauge+delta",
+    value = 27,
+    delta = list(reference = 31),
+    domain = list(x = c(0.25, 1), y = c(0.7, 0.9)),
+    title = list(text = "Goal 13 Completion"),
+    gauge = list(
+      shape = "bullet",
+      axis = list(range = list(NULL, 200)),
+      threshold = list(
+        line = list(color = "red", width = 2),
+        thickness = 0.75,
+        value = 27),
+      steps = list(
+        list(range = c(0, 150), color = "lightgray"),
+        list(range = c(150, 200), color = "gray")),
+      bar = list(color = "green")))
+
+goal_4
---
+++ individual_bulletcharts.R
@@ -0,0 +1,54 @@
+
+library(plotly)
+#goal 13 individual code
+
+goals_1<- plot_ly(
+  type = "indicator",
+  mode = "number+gauge+delta",
+  value = 27,
+  domain = list(x = c(0, 1), y= c(0, 1)),
+  title = list(text = "<b>Goal 13 
+                    completions</b>"),
+  delta = list(reference = 31),
+  gauge = list(
+    shape = "bullet",
+    axis = list(range = list(NULL, 200)),
+    threshold = list(
+      line = list(color = "red", width = 2),
+      thickness = 0.75,
+      value = 31),
+    steps = list(
+      list(range = c(0, 150), color = "lightgray"),
+      list(range = c(150, 200), color = "gray"))),
+  height = 150, width = 600) 
+goals_1 <- goals_1 %>%
+  layout(margin = list(l= 150, r= 10))
+
+goals_1
+
+
+#goal 17 individual code 
+
+goals_2<- plot_ly(
+  type = "indicator",
+  mode = "number+gauge+delta",
+  value = 43,
+  domain = list(x = c(0, 1), y= c(0, 1)),
+  title = list(text = "<b>Goal 17 
+                    completions</b>"),
+  delta = list(reference = 40),
+  gauge = list(
+    shape = "bullet",
+    axis = list(range = list(NULL, 200)),
+    threshold = list(
+      line = list(color = "red", width = 2),
+      thickness = 0.75,
+      value = 40),
+    steps = list(
+      list(range = c(0, 150), color = "lightgray"),
+      list(range = c(150, 200), color = "gray"))),
+  height = 150, width = 600) 
+goals_2 <- goals_2 %>%
+  layout(margin = list(l= 150, r= 10))
+
+goals_2
