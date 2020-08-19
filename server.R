d3f3cbe55ed975b8df6dcb040e4812b8da8c762f
Jack Ogilvie-Richards <jackogilvierichards.work@users.noreply.github.com>
2020-08-19 15:44:32 +0100

    deletion of shinydashboard files; ui, global and server updated to use shinydashboard code, working

--- server.R
+++ server.R
@@ -6,13 +6,13 @@ server <- function(input, output) {
     
     referrers %>%
       mutate(fullReferrer = recode(fullReferrer, "forms.zohopublic.eu/codeclan/form/ProfessionalSoftwareDevelopmentcourse/formperma/YQR5YuzRB8ROJrjctYuwRCX9yzeUQF7uZtNJb4rrwDo"
-                                 = "forms.zohopublic.eu", "gstatic.com/atari/embeds/913211048dfa67f4be7864f4505a4b63/intermediate-frame-minified.html"
-                                 = "gstatic.com", "m.facebook.com/" = "social media", "linkedin.com/" = "social media",
-                                 "linkedin.com/feed/" = "social media", "facebook.com/" = "social media", "lm.facebook.com/l.php" = "social media",
-                                 "lm.facebook.com" = "social media", "lnkd.in" = "social media", "I.instrgram.com" = "social media",
-                                 "instagram.com/" = "social media", "linkedin.com/school/codeclan" = "social media",
-                                 "l.facebook.com/" = "social media", "lm.facebook.com/" = "social media", "lnkd.in/" = "social media",
-                                 "l.instagram.com/" = "social media")) %>%
+                                   = "forms.zohopublic.eu", "gstatic.com/atari/embeds/913211048dfa67f4be7864f4505a4b63/intermediate-frame-minified.html"
+                                   = "gstatic.com", "m.facebook.com/" = "social media", "linkedin.com/" = "social media",
+                                   "linkedin.com/feed/" = "social media", "facebook.com/" = "social media", "lm.facebook.com/l.php" = "social media",
+                                   "lm.facebook.com" = "social media", "lnkd.in" = "social media", "I.instrgram.com" = "social media",
+                                   "instagram.com/" = "social media", "linkedin.com/school/codeclan" = "social media",
+                                   "l.facebook.com/" = "social media", "lm.facebook.com/" = "social media", "lnkd.in/" = "social media",
+                                   "l.instagram.com/" = "social media")) %>%
       arrange(desc(sessions)) %>%
       group_by(fullReferrer) %>%
       summarise(sessions = sum(sessions)) %>%
@@ -31,9 +31,9 @@ server <- function(input, output) {
             axis.title.x = element_text(size = 20),
             axis.text = element_text(size = 15),
             axis.title.y = element_text(size = 20))
-     
+    
   })
-
+  
   
   output$social <- renderPlot({
     
@@ -55,7 +55,7 @@ server <- function(input, output) {
             axis.title.x = element_text(size = 20),
             axis.text = element_text(size = 15),
             axis.title.y = element_text(size = 20))
-  
+    
   })
   
   output$landing <- renderPlot({
@@ -152,9 +152,9 @@ server <- function(input, output) {
             axis.title.y = element_text(size = 20),
             legend.title = element_text(size = 20),
             legend.text = element_text(size = 15))
-  
-
+    
+    
   })
-
- 
+  
+  
 } 
--- ui.R
+++ ui.R
@@ -1,42 +1,61 @@
 
-ui <- fluidPage(theme = shinytheme("spacelab"),
-                
-  titlePanel(tags$h1("CodeClan Website Navigation")),
-    
-    mainPanel(
-      
-      tabsetPanel(
-        
-        tabPanel("How do visitors find CodeClan",
-                
-                plotOutput("referrers"),
-                
-                plotOutput("social")
-                          
-        ),
-        
-        tabPanel("Landing Pages",
-                 
-                plotOutput("landing"),
-                
-                textOutput("landing_text")
-                
-                 
-        ),
-        
-        tabPanel("Goal Completion",
-                 
-                 plotOutput("goals"),
-                 
-                 plotOutput("landing_goals17"),
-                 
-                 plotOutput("landing_goals13")
-                 
-        )
-        
-      )
-      
-    )
- 
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("How do visitors find CodeClan?", tabName = "social_location", icon = icon("dashboard")),
+    menuItem("Landing Page", icon = icon("dashboard"), tabName = "landing_page",
+             badgeLabel = "new", badgeColor = "green"),
+    menuItem("valueBox test", tabName = "valueboxtest", icon = icon("thumbs-up", lib = "glyphicon"))
   )
-  
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "social_location",
+            box(width = 12,
+                plotOutput("goals")
+            )
+            
+            
+            ,
+            
+            box(width = 12,
+                plotOutput("social")
+            ),
+            
+            box(width = 8,
+                plotOutput("referrers")
+            ),
+            
+            box(width = 4,
+                valueBox(goals$goal17Completions[length(goals)] + goals$goal13Completions[length(goals)], "Total webinar signups this month", icon = icon("list"))
+            )
+            
+    ),
+    
+    
+    
+    tabItem(tabName = "landing_page",
+            box(width = 12,
+                plotOutput("landing")
+            ),
+            box(width = 12,
+                plotOutput("landing_goals13")
+            ),
+            box(width = 12,
+                plotOutput("landing_goals17")
+            )
+    ),
+    
+    tabItem(tabName = "valueboxtest",
+            valueBox(50*2, "valueBox test", icon = icon("list")))
+  )
+)
+
+
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Website Navigation Results"),
+  sidebar,
+  body
+)
